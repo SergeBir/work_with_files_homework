3921382ce1cbcfa8b96c0920ca4a0d526aeb4c18
Sergei Biryukov <sergebir.work@gmail.com>
2023-03-08 18:02:30 +0300

    second exercise

--- homework.py
+++ homework.py
@@ -13,11 +13,11 @@ class WorkWithFiles:
                 ingredient_count = int(file.readline())
                 ingredients = []
                 for _ in range(ingredient_count):
-                    products, count, size_form = file.readline().strip().split(" | ")
+                    products, quantity, measure = file.readline().strip().split(" | ")
                     ingredients.append({
                         "ingredient_name": products,
-                        "quantity": count,
-                        "measure": size_form,
+                        "quantity": quantity,
+                        "measure": measure,
                     })
                 cook_book[name] = ingredients
                 file.readline()
@@ -25,7 +25,26 @@ class WorkWithFiles:
                 print(f"{name}:")
                 pprint(ingredients, sort_dicts=False)
 
+    def get_shop_list_by_dishes(self, dishes, person_count):
+        with open("recipes.txt", "r") as file:
+            cook_book = {}
+            for line in file:
+                name = line.strip()
+                ingredient_count = int(file.readline())
+                ingredients = {}
+                for _ in range(ingredient_count):
+                    products, quantity, measure = file.readline().strip().split(" | ")
+                    ingredients[products] = {
+                            "quantity": int(quantity) * person_count,
+                            "measure": measure,
+                        }
+                cook_book[name] = ingredients
+                file.readline()
+            for name in cook_book.keys():
+                if name in dishes:
+                    pprint(cook_book[name])
 
 
 new_work = WorkWithFiles()
 new_work.files_manipulation()
+new_work.get_shop_list_by_dishes(["Запеченный картофель", "Омлет"],2)
